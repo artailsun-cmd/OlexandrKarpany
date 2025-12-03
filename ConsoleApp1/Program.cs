003120e7d2eaffc52b530ff441137b8bb98c6bba
AlexKarpany <artailsun@gmail.com>
2025-12-03 17:51:40 +0200

    1st_code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,53 @@
-﻿
+﻿string? name = "user";
+string productname = "produckt";
+double price = 22.50;
+string address = "strret";
+int housenomber = 25;
+
+Console.WriteLine("Order 1. Write name: ");
+name = Console.ReadLine();
+Console.WriteLine("Produckt name: ");
+productname = Console.ReadLine();
+Console.WriteLine("Produckt price: ");
+price = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Address. Wrire street name: ");
+address = Console.ReadLine();
+Console.WriteLine("House number: ");
+housenomber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Order #1");
+Console.WriteLine($"Client: {name}.");
+Console.WriteLine($"Product: {productname}, price {price} EUR.");
+Console.WriteLine($"Address: {address} {housenomber}.");
+
+Console.WriteLine("Order 2. Write name: ");
+name = Console.ReadLine();
+Console.WriteLine("Produckt name: ");
+productname = Console.ReadLine();
+Console.WriteLine("Produckt price: ");
+price = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Address. Wrire street name: ");
+address = Console.ReadLine();
+Console.WriteLine("House number: ");
+housenomber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Order #2");
+Console.WriteLine($"Client: {name}.");
+Console.WriteLine($"Product: {productname}, price {price} EUR.");
+Console.WriteLine($"Address: {address} {housenomber}.");
+
+Console.WriteLine("Order 3. Write name: ");
+name = Console.ReadLine();
+Console.WriteLine("Produckt name: ");
+productname = Console.ReadLine();
+Console.WriteLine("Produckt price: ");
+price = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Address. Wrire street name: ");
+address = Console.ReadLine();
+Console.WriteLine("House number: ");
+housenomber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Order #1");
+Console.WriteLine($"Client: {name}.");
+Console.WriteLine($"Product: {productname}, price {price} EUR.");
+Console.WriteLine($"Address: {address} {housenomber}.");
